4c6b37ab152f0457680448e6b41f031bcb6196b5
Noe <noe@gmail.com>
2023-06-02 21:26:36 -0500

    Cruds Product, Customer and Invoice

---
+++ CodeFirstDemo/Models/Invoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstDemo.Models
+{
+    public class Invoice
+    {
+        public int InvoiceID { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int CustomerID { get; set; }
+        public Customer Customer { get; set; }
+        public List<Detail> Details { get; set; }
+    }
+}
